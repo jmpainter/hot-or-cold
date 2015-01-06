390020b9576e135e4d744034dc4198a4f86c09e8
Josh Painter <jpainter4@gmail.com>
2015-01-05 22:44:17 -0800

    created game logic

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
-
 $(document).ready(function(){
-	
+
+	var DEBUG_MODE = true;
+	var number;
+	var prevGuess;
+	var numGuesses;
+
+	var debug = function(msg) {
+		if (DEBUG_MODE = true) {
+			console.log('DEBUG:', msg);
+		}
+	}
+
+	function newGame() {
+		debug('newgame called');
+		setNumber();
+		prevGuess = 0;
+		numGuesses = 0;
+		$('#guessList').empty();
+		$('#feedback').text('Make your Guess!');
+		$('#count').text('0');
+		$('#userGuess').val('');
+	}
+	newGame();
+
+	function setNumber() {
+		number = Math.floor((Math.random() * 100) + 1);
+		debug('number: ' + number);
+	}	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,6 +38,70 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+	$('.new').click(function() {  
+		newGame();
+	});
+
+	$('#guessButton').click(function () {
+		var guess = $('#userGuess').val();
+		if (isNaN(guess)) 
+		{
+			alert('Please enter a number');
+			return false;
+		}	
+		if(guess % 1 != 0) {
+			alert('Please enter an integer');
+			return false;
+		}
+		if(guess < 1 || guess > 100) {
+			alert('Please enter a number between 1 and 100');
+		}
+		$('#count').text(++numGuesses);
+		$('#feedback').text(getGuessResult(guess));		
+		prevGuess = guess;			
+		$('#guessList').append('<li>' + guess + '</li>');
+
+	});
+
+	function getGuessResult(guess) {
+		debug('logGuess called. number: ' + number + ' guess: ' + guess + ' prevGuess: ' + prevGuess + ' guessDifference: ' + Math.abs(number - guess));
+		var guessResult = "";
+		if(prevGuess != 0){
+			if (Math.abs(number - guess) == 0) {
+				guessResult = "You Got It!";
+			}
+			else if(Math.abs(number - guess) < Math.abs(number - prevGuess)) {
+				guessResult = "Warmer";
+				
+			}
+			else {
+				guessResult = "Colder";
+			}	
+		}
+		else {
+			if (Math.abs(number - guess) >= 50) {
+				guessResult = "Ice Cold";
+			}
+			else if (Math.abs(number - guess) >= 30) {
+				guessResult = "Cold";
+			}
+			else if (Math.abs(number - guess) >= 20) {
+				guessResult = "Warm";
+			}
+			else if (Math.abs(number - guess) >= 10) {
+				guessResult = "Hot";
+			}
+			else if(Math.abs(number - guess) >= 1) {
+				guessResult = "Very Hot";
+			}
+			else {
+				guessResult = "You Got It!";
+			}
+		}
+		return guessResult;
+		
+	}
 });
 
 
